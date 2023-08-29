c9cd9832cecc9a6272a0d3562742c79e4371e640
Ivan Popov <tyrael2101@hotmail.com>
2023-08-29 14:06:11 +0200

    message

---
+++ ivanpopov.js
@@ -0,0 +1,16 @@
+// En bog fra Saxo shop
+
+const book = {
+    name: "The Complete Sherlock Holmes",
+    author: "Arthur Conan Doyle",
+    language: "English",
+    pages: 1200,
+    publish_date: "15-10-2019",
+    price: 219.95
+}
+
+// Vi kan nu tilgå forskellige properties f.eks bogets navn
+let bookName = book.name;
+
+// fortfatter
+let bookAuthor = book.author;
